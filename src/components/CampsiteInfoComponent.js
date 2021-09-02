2d32ea717feae3cb9216b5cf85524ade5ca0ae7d
Asel Atakeev <aselya.atakeeva@gmail.com>
2021-09-02 11:01:34 -0700

    added a new file

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -12,6 +12,7 @@ class CampsiteInfo extends Component {
                      <CardText>{campsite.description}</CardText>
                   </CardBody>
             </Card>
+            
          </div>
       );
 }
