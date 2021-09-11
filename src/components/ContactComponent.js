5486ccb8483e012f26e3a9ea5175c1f98d045be6
Asel Atakeev <aselya.atakeeva@gmail.com>
2021-09-11 13:07:41 -0700

    "added validators"

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -4,6 +4,7 @@ import { Breadcrumb, BreadcrumbItem,
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 
+
 const required = val => val && val.length; //functions
 const maxLength = len => val => !val || (val.length <= len);
 const minLength = len => val => val && (val.length >= len);
